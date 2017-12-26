d0796e54ddf50fda3b238149bcecce84be04ee4b
ZawZaw <mczawzaw12@outlook.com>
2017-12-26 20:01:33 +0630

    app: Add Book Reviews Section
    Signed-off-by:    ZawZaw <mczawzaw12@outlook.com>

---
+++ app/src/main/java/com/zawzaw/savethelibrary/adapter/ReviewsAdapter.java
@@ -0,0 +1,11 @@
+package com.zawzaw.savethelibrary.adapter;
+
+/**
+ * Created by zawzaw on 26/12/17.
+ */
+
+public class ReviewsAdapter
+{
+
+
+}
---
+++ app/src/main/java/com/zawzaw/savethelibrary/viewmodel/ReviewsViewHolder.java
@@ -0,0 +1,18 @@
+package com.zawzaw.savethelibrary.viewmodel;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+/**
+ * Created by zawzaw on 26/12/17.
+ */
+
+public class ReviewsViewHolder extends RecyclerView.ViewHolder
+{
+    public ReviewsViewHolder(View itemView)
+    {
+        super(itemView);
+
+    }
+
+}
