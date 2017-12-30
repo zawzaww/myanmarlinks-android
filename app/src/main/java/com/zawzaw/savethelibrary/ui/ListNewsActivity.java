8238c2de9ee58475794c1ae0377ba5103216d469
ZawZaw <mczawzaw12@outlook.com>
2017-12-30 22:51:15 +0630

    Activity: Add List activities
    Signed-off-by:    ZawZaw <mczawzaw12@outlook.com>

--- app/src/main/java/com/zawzaw/savethelibrary/ui/ListNewsActivity.java
+++ app/src/main/java/com/zawzaw/savethelibrary/ui/ListNewsActivity.java
@@ -4,25 +4,28 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import com.zawzaw.savethelibrary.R;
 
-public class PdfDetailsActivity extends AppCompatActivity
+public class ListNewsActivity extends AppCompatActivity
 {
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_pdf_details);
+        setContentView(R.layout.activity_list_news);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view)
-            {
+            public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
-
             }
         });
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
--- app/src/main/java/com/zawzaw/savethelibrary/ui/ListReviewActivity.java
+++ app/src/main/java/com/zawzaw/savethelibrary/ui/ListReviewActivity.java
@@ -4,16 +4,20 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import com.zawzaw.savethelibrary.R;
 
-public class PdfDetailsActivity extends AppCompatActivity
+public class ListReviewActivity extends AppCompatActivity
 {
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_pdf_details);
+        setContentView(R.layout.activity_list_review);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -22,7 +26,6 @@ public class PdfDetailsActivity extends AppCompatActivity
             {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
-
             }
         });
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
