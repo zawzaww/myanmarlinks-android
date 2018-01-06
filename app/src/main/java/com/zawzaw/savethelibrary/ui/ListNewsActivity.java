f0f4aba13cb421fa597d2329fdbe7e3a010cb35e
ZawZaw <mczawzaw12@outlook.com>
2018-01-06 11:37:02 +0630

    SaveTheLibrary: Layout: Add Activities
    Signed-off-by:    ZawZaw <mczawzaw12@outlook.com>

--- app/src/main/java/com/zawzaw/savethelibrary/ui/ListNewsActivity.java
+++ app/src/main/java/com/zawzaw/savethelibrary/ui/ListNewsActivity.java
@@ -4,15 +4,18 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import com.zawzaw.savethelibrary.R;
 
-public class PdfDetailActivity extends AppCompatActivity {
+public class ListNewsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_pdf_detail);
+        setContentView(R.layout.activity_list_news);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
